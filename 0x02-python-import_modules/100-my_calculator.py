967b146bf40289ab1098cf5f2b6b647ee1c4d819
Math-tad <mathi.tadesse@gmail.com>
2022-06-30 12:21:49 -0700

    Import a function and work with args

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    from calculator_1 import add, sub, mul, div
+    num_of_arg = len(argv[1:])
+    if num_of_arg != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    else:
+        for i, arg in enumerate(argv[1:]):
+            if i = 0:
+                arg1 = int(arg)
+            elif i == 1:
+                arg2 = arg
+            elif i == 2:
+                arg3 = int(arg)
+        if arg2 == '+':
+            value = add(arg1, arg3)
+        elif arg2 == '-':
+            value = sub(arg1, arg3)
+        elif arg2 == '*':
+            value = mul(arg1, arg3)
+        elif arg2 == '/':
+            value = div(arg1, arg3)
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
+    print("{:d}: {:s} {:d} = {:d}".format(arg1, arg2, arg3, value))
