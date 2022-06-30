b7648a615a0c30154e9009fa211cf6b9de498395
Math-tad <mathi.tadesse@gmail.com>
2022-06-30 09:57:58 -0700

    Number of arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    num_of_arg = len(argv[1:])
+    if num_of_arg == 0:
+        print("{:d} arguments.".format(num_of_arg))
+    else:
+        if leng == 1:
+            print("{:d} argument:".format(num_of_arg))
+        else:
+            print("{:d} arguments:".format(num_of_arg))
+        for i, arg in enumerate(argv[1:]):
+            print("{:d}: {:s}".format(i + 1, arg))
