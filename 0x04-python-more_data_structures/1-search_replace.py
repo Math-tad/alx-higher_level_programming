b381ee8caab70c1ed51e961a1370ec9c06a24d81
Math-tad <mathi.tadesse@gmail.com>
2022-07-06 14:00:24 -0700

    Find and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = [] 
+    new_list = []
     if my_list is not None:
         for i in my_list:
             if i == search:
