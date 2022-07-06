2a11ed728418ba0802dbbcbe9616ebbc3c13887f
Math-tad <mathi.tadesse@gmail.com>
2022-07-06 13:44:47 -0700

    Square value of an integer

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = [] 
+    new_matrix = []
     if matrix is not None:
         for i in matrix:
             square = lambda j: j ** 2
